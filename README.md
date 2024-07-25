571e1d1d4de839ca8640a3b0981e1c322af3d4d5
David Mendiola <jdmmendiola@gmail.com>
2024-07-25 07:20:10 -0400

    add readme

--- README.md
+++ README.md
@@ -2,6 +2,51 @@
 
 ## Installation Steps
 
-run npm install in root directory
+0. Sending DB via email it's a flat file
 
-run npm install in client directory
+1. run `npm install` on the root directory
+
+2. run `npm install` in the client directory
+
+3. run `npm run dev` on the root directory this will run the back-end /src/app.ts file
+
+4. Usually you would run react via its own server but started this in a rush and did not research enough so I'm just serving the final build folder from react to express.js. You'll need to run `npm run build` in the client folder. You should be running the backend from #3 and this step will load react
+
+5. Access the page via the express home route `http://localhost:3000`
+
+### About the Code
+
+Database Model
+
+- See my model in the root Assets folder diagram-export....
+
+Development Stack
+
+- Node.js and Express.js running the back-end routes
+- SQLite database since it was a simple flat file
+- Drizzle ORM to interface with the database and create models
+- React created with Vite
+- The whole project supports TS
+- Deployed via DockerFile to fly.io server host
+
+app.ts has all the back-end routes its messy :) You'll see some commented code at the top to populate the database
+
+React Code
+
+- Really messy in here but here's the hierarchy
+
+1. main.jsx calls routes folder -> root.jsx and then you'll see `<Header> and <Columns>` columns has all the code dumped in there broken down into component the main bulk is in the components folder
+
+### Possible Features
+
+- Currently I'm hard coding session which essentially is the "card" or "tile" rounded corner in white that you see in `line 254` of app.ts you'll see I pick 2 as I hard coded only 2 sessions in the database. Ideally a user would "create" a new session first thus creating a new ID in the database and a new tile. This could be a button or a 24 range from the time they possibly click a button that says "Start New Session"
+
+- If you add a workout again you'll see it will append to the second card / tile at the bottom
+
+- Form validation in the launch workout page
+
+- The tiles animation card don't work I was just playing around with some toggle and animations in react
+
+- Maybe add authentication / login page of course
+
+- Anything you think :)
--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,7 @@ import path from 'path';
 
 //const sqlite = new Database('/data/nova1.db');
 const sqlite = new Database('./novatest.db');
+console.log(sqlite);
 const db = drizzle(sqlite);
 migrate(db, { migrationsFolder: './migrations' });
 
@@ -23,6 +24,167 @@ app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
+
+// // /* sanitize tables */
+// db.delete(users).run();
+// db.delete(workouts).run();
+// db.delete(exercises).run();
+// db.delete(userSessions).run();
+// db.delete(workoutSession).run();
+
+// /* fill users */
+
+// db.insert(users)
+//   .values({
+//     name: 'David Mendiola',
+//     email: 'jdmmendiola@gmail.com',
+//   })
+//   .run();
+
+// /* fill exercises */
+// db.insert(exercises)
+//   .values([
+//     {
+//       id: 1,
+//       name: 'Bench Press',
+//     },
+//     {
+//       id: 2,
+//       name: 'Dead Lift',
+//     },
+//     {
+//       id: 3,
+//       name: 'Squats',
+//     },
+//     {
+//       id: 4,
+//       name: 'Front Squats',
+//     },
+//     {
+//       id: 5,
+//       name: 'Chest Flys',
+//     },
+//     {
+//       id: 6,
+//       name: 'Lat Pulldown',
+//     },
+//     {
+//       id: 7,
+//       name: 'Face Pulls',
+//     },
+//     {
+//       id: 8,
+//       name: 'Bicep Curls',
+//     },
+//     {
+//       id: 9,
+//       name: 'Tricep Extensions',
+//     },
+//     {
+//       id: 10,
+//       name: 'Leg Press',
+//     },
+//     {
+//       id: 11,
+//       name: 'Plank',
+//     },
+//     {
+//       id: 12,
+//       name: 'Hamstring Curls',
+//     },
+//   ])
+//   .run();
+
+// /* fill workouts */
+
+// db.insert(workouts)
+//   .values([
+//     { id: 1, exerciseId: 1, reps: 12, set: 1, weight: 185 },
+//     { id: 2, exerciseId: 1, reps: 10, set: 2, weight: 190 },
+//     { id: 3, exerciseId: 1, reps: 8, set: 3, weight: 200 },
+//     { id: 4, exerciseId: 2, reps: 12, set: 1, weight: 180 },
+//     { id: 5, exerciseId: 2, reps: 10, set: 2, weight: 200 },
+//     { id: 6, exerciseId: 2, reps: 8, set: 3, weight: 225 },
+//     { id: 7, exerciseId: 9, reps: 15, set: 1, weight: 80 },
+//     { id: 8, exerciseId: 9, reps: 12, set: 2, weight: 90 },
+//     { id: 9, exerciseId: 9, reps: 6, set: 3, weight: 110 },
+//     { id: 10, exerciseId: 8, reps: 15, set: 1, weight: 80 },
+//     { id: 11, exerciseId: 7, reps: 12, set: 1, weight: 90 },
+//     { id: 12, exerciseId: 5, reps: 6, set: 1, weight: 110 },
+//   ])
+//   .run();
+
+// /* fill userSessions */
+// db.insert(userSessions)
+//   .values([
+//     {
+//       id: 1,
+//       userId: 1,
+//       location: 'Fit Squad Toronto',
+//       status: 'Amazing',
+//     },
+//     {
+//       id: 2,
+//       userId: 1,
+//       location: 'Fit Squad Toronto',
+//       status: 'Meh',
+//     },
+//   ])
+//   .run();
+
+// /* workoutSession */
+// db.insert(workoutSession)
+//   .values([
+//     {
+//       userSessionId: 1,
+//       workoutsId: 1,
+//     },
+//     {
+//       userSessionId: 1,
+//       workoutsId: 2,
+//     },
+//     {
+//       userSessionId: 1,
+//       workoutsId: 3,
+//     },
+//     {
+//       userSessionId: 1,
+//       workoutsId: 4,
+//     },
+//     {
+//       userSessionId: 1,
+//       workoutsId: 5,
+//     },
+//     {
+//       userSessionId: 1,
+//       workoutsId: 6,
+//     },
+//     {
+//       userSessionId: 1,
+//       workoutsId: 7,
+//     },
+//     {
+//       userSessionId: 1,
+//       workoutsId: 8,
+//     },
+//     {
+//       userSessionId: 1,
+//       workoutsId: 9,
+//     },
+//     {
+//       userSessionId: 2,
+//       workoutsId: 10,
+//     },
+//     {
+//       userSessionId: 2,
+//       workoutsId: 11,
+//     },
+//     {
+//       userSessionId: 2,
+//       workoutsId: 12,
+//     },
+//   ])
+//   .run();
 
 app.get('/api', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
